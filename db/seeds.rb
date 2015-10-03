b2c19ed17e0e4153fb8d1df079bbf52b119b92a8
Takamichi-tsutsumi <bdgaqh1889@gmail.com>
2015-10-03 22:08:02 +0900

    heroku seed orz

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,22 +35,22 @@ PublicManifest.create(
 )
 p "done"
 
-p "-- create a question_set"
-QuestionSet.create(
-	title: "質問セット1",
-	q1_id: 1,
-	q2_id: 1,
-	q3_id: 1,
-	q4_id: 1,
-	q5_id: 1,
-	q6_id: 1,
-	q7_id: 1,
-	q8_id: 1,
-	q9_id: 1,
-	q10_id: 1,
-	status: 1
-)
-p "done"
+# p "-- create a question_set"
+# QuestionSet.create(
+# 	title: "質問セット1",
+# 	q1_id: 1,
+# 	q2_id: 1,
+# 	q3_id: 1,
+# 	q4_id: 1,
+# 	q5_id: 1,
+# 	q6_id: 1,
+# 	q7_id: 1,
+# 	q8_id: 1,
+# 	q9_id: 1,
+# 	q10_id: 1,
+# 	status: 1
+# )
+# p "done"
 
 p "--create a Political Party"
 PoliticalParty.create(
